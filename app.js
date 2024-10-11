fff99c581c1401bbc963704d985ef124951d3584
Rodrigo Joaquim <rjgalmeida@gmail.com>
2024-10-11 20:02:36 -0300

    My first API Project using Postgres DB with best anti SQL Injection practice

---
+++ app.js
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import express from 'express';
+import userRouter from './routes/userRoutes.js';
+
+const app = express();
+
+app.use('/users', userRouter);
+
+app.listen(3000, () => console.log('API ecommerce ONLINE'));
